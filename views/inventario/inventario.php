897d71007c9bc1da8d05df128513bf82d7c3f4de
andres <agudeloandres@gmail.com>
2020-08-15 16:44:08 -0500

    nueva version

---
+++ views/inventario/inventario.php
@@ -0,0 +1,55 @@
+<html>
+<style type="text/css">
+
+.borde{ border-top: 30px;
+
+
+
+}
+
+</style>
+<head>
+	<title></title>
+</head>
+<body>
+
+<script src="../../assets/node_modules/jquery/dist/jquery.min.js" ></script>
+<link rel="stylesheet" href="../../assets/node_modules/bootstrap/dist/css/bootstrap.min.css">
+<script src="../../assets/node_modules/bootstrap/dist/js/bootstrap.min.js" ></script>
+
+
+
+<div class="container">
+  <div class=" borde row">
+    <div class="col-md-4 border border-secondary rounded-bottom text-center text-uppercase ">
+      caracteristicas
+      <p>
+        <a class="btn btn-primary" data-toggle="collapse" href="#hola" aria-expanded="false" aria-controls="collapseExample">
+          resumen
+        </a>
+        <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+          detalles
+        </button>
+      </p>
+      <div class="collapse" id="collapseExample">
+        <div class="card card-body">
+          hola
+        </div>
+      </div>  
+      <div class="collapse" id="hola">
+        <div class="card card-body">
+          holassss
+        </div>
+      </div>  
+    </div>
+    <div class="col-md-8 border border-secondary rounded-bottom text-center text-uppercase">
+      aca
+   </div>
+  </div>
+</div>
+
+
+
+
+</body>
+</html>
