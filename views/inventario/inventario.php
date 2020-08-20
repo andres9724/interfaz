c1fcaa4f6b3ce0a218602c697dd5b5dc98bd273c
andres <agudeloandres@gmail.com>
2020-08-19 20:40:48 -0500

    inventario 3.0

--- views/inventario/inventario.php
+++ views/inventario/inventario.php
@@ -1,55 +1,147 @@
 <html>
-<style type="text/css">
 
-.borde{ border-top: 30px;
-
-
-
-}
-
-</style>
 <head>
 	<title></title>
 </head>
-<body>
+
 
 <script src="../../assets/node_modules/jquery/dist/jquery.min.js" ></script>
 <link rel="stylesheet" href="../../assets/node_modules/bootstrap/dist/css/bootstrap.min.css">
 <script src="../../assets/node_modules/bootstrap/dist/js/bootstrap.min.js" ></script>
 
+<script src='https://kit.fontawesome.com/a076d05399.js'></script>
+
+<style type="text/css">
+
+body{
+ 
+ padding-top: 11em;
+}
 
 
+
+
+
+</style>
+<body background="../images/thumb-1920-722604.jpg" >
 <div class="container">
   <div class=" borde row">
     <div class="col-md-4 border border-secondary rounded-bottom text-center text-uppercase ">
       caracteristicas
       <p>
-        <a class="btn btn-primary" data-toggle="collapse" href="#hola" aria-expanded="false" aria-controls="collapseExample">
-          resumen
+        <a class="btn btn-secondary" data-toggle="collapse" href="#hola" aria-expanded="true" aria-controls="collapseExample" style="width:150px; height:25px ;font-size:12px">
+         resumen
         </a>
-        <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+
+        <button class="btn btn-secondary text-uppercase" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample" style="width:150px; height:25px ;font-size:12px">
           detalles
         </button>
       </p>
       <div class="collapse" id="collapseExample">
         <div class="card card-body">
-          hola
+          
+           <div class="col-md-12 border border-secondary  text-center text-uppercase ">
+            <div class="row">
+              <div class="col-md-5 border border-secondary  text-center text-uppercase ">
+                <img src="../images/images.jpg"  style="width:100px; height:100px ">
+              </div>
+              <div class="col-md-7  rounded-bottom text-center text-uppercase ">
+               <table style="font-size:12px">
+                <tr>
+                <td class="text-lowercase font-weight-bold">%NOMBRE%</td>
+              </tr>
+              <tr>
+                <td>NIVEL <i>%nivel</i></td>
+              </tr>
+              <tr>
+                <td class="fas fa-chess-queen text-warning">%puntos%<i>:puntos</i></td>
+              </tr>
+
+               </table> 
+              </div>
+            </div>gremio
+          </div><br>
+          <div class="col-md-12">
+            <div class="row">
+
+              <div class="col-md-4"  style="padding-left:0px; padding-right:0px">
+                <tr>Experiencia:</tr><br>
+                <tr>Energia:</tr>                
+              </div>
+
+              <div class="col-md-8">
+                
+                <div class="progress" >
+                  <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 50%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100" style="padding-left:0px; "></div>
+                </div>
+                <br>
+                
+                <div class="progress" >
+                  <div class="progress-bar progress-bar-striped" role="progressbar" style="width: 90%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100" style="padding-left:0px; "></div>
+                </div>
+             
+              </div>
+
+            </div>
+          </div>   
         </div>
       </div>  
       <div class="collapse" id="hola">
         <div class="card card-body">
-          holassss
+          
         </div>
       </div>  
     </div>
-    <div class="col-md-8 border border-secondary rounded-bottom text-center text-uppercase">
-      aca
-   </div>
+
+    <div class="col-md-8 border border-secondary rounded-bottom text-center text-uppercase ">
+      inventario
+      <div class="container">
+        <div class=" borde row">
+          <div class="col-md-8 border border-secondary rounded-bottom text-center text-uppercase ">
+            caracteristicas
+          </div>
+          <div class="col-md-4 border border-secondary rounded-bottom text-center text-uppercase">
+            <div class="container">
+              <div class=" borde row">
+                <div class="col-md-2   " style="padding-left:0px; padding-right:0px">
+                  <button  class="fas fa-retweet" style="font-size:18px "></button>
+                </div>
+                <div class="col-md-2   " style="padding-left:0px; padding-right:0px">
+                  <button class="fas fa-wrench" style="font-size:18px "></button>
+                </div>
+                <div class="col-md-2   " style="padding-left:0px; padding-right:0px;margin-left: -5">
+                  <button  class="fas fa-wine-glass" style="font-size:18px"></button>  
+                </div>
+                <div class="col-md-2   " style="padding-left:0px; padding-right:0px;margin-left: -5">
+                  <button  class="fas fa-hat-wizard" style="font-size:18px "></button>
+                </div>
+                <div class="col-md-2   " style="padding-left:0px; padding-right:0px;margin-left: 2">
+                    <button  class="fas fa-key " style="font-size:18px "></button> 
+                </div>
+                <div class="col-md-2   " style="padding-left:0px; padding-right:0px">
+                  <button class="fas fa-puzzle-piece" style="font-size:18px "></button>
+                </div>
+                <div class="btn-group">
+                  <button class="btn btn-secondary btn-sm text-uppercase" type="button" style="width:150px; height:25px  ">
+                    categorias
+                  </button>
+                  <button type="button" class="btn btn-sm btn-secondary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span class="sr-only">Toggle Dropdown</span>
+                  </button>
+                  <div class="dropdown-menu">
+                    ...
+                  </div><button  class="fas fa-cog" style="font-size:100%; color:orange " type="submit" ></button>
+                </div>
+
+                      
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
-
-
-
 
 </body>
 </html>
